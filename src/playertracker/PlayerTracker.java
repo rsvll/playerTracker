c05b5f470d40e8fe8cec338ae47e8e60e2b9a824
Richard Sevilla <svll.nyc@gmail.com>
2019-05-08 21:24:34 -0400

    search for item by key

--- src/playertracker/PlayerTracker.java
+++ src/playertracker/PlayerTracker.java
@@ -15,7 +15,52 @@ public class PlayerTracker {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        long[] arr;                 //references array
+        arr = new long[100];        // make array
+        int nElems = 0;             // number of items 
+        int j;                      // loop counter
+        long searchKey;             // key item to search for
+        
+    //==========================================================================
+
+    arr[0] = 77;                    // insert 10 items
+    arr[1] = 99;
+    arr[2] = 23;
+    arr[3] = 24;
+    arr[4] = 30;
+    arr[5] = 66;
+    arr[6] = 00;
+    arr[7] = 10;
+    arr[8] = 54;
+    arr[9] = 33;
+    nElems = 10;                    // now 10 items in array 
+    
+    //==========================================================================
+    
+    for (j = 0; j < nElems; j++ ){     // Display Items
+        System.out.println(arr[j] + " ");
+        System.out.println("");
+    }
+    
+    //==========================================================================
+    
+    searchKey = 23;                                            // find item with key 23
+    
+    for( j = 0; j < nElems; j++){                              // for each element 
+        if( arr[j] == searchKey)                                // Found item
+            break;                                              // yes, exit before end
+        if( j == nElems)                                       // at the end?
+            System.out.println("Cant find " + searchKey);      // yes
+        else
+            System.out.println("Found " + searchKey);          // no 
+        
+    }
+    
+    //===========================================================================
+    
+    searchKey = 54;                                             // Delete item with key 55
+    
     }
     
 }
